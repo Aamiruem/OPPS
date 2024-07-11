097307cc036741390f4aa0687067d08c084588a7
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-12 00:16:30 +0530

    commit add

--- L5GetterSetter/Product.java
+++ L5GetterSetter/Product.java
@@ -7,60 +7,58 @@ public class Product {
     private double price;
 
     // public void setProduct(int i, String s, double p) {
-    //     id = i;
-    //     name = s;
-    //     price = p;
+    // id = i;
+    // name = s;
+    // price = p;
 
-    //     if (i < 0){
-    //     System.out.println("id can not be negative");
-    //     return;
+    // if (i < 0){
+    // System.out.println("id can not be negative");
+    // return;
 
-    //     } else if (price <= 0) {
-    //         System.out.println("price can not be negative");
-    //         return;
+    // } else if (price <= 0) {
+    // System.out.println("price can not be negative");
+    // return;
 
-    //     } else {
-    //         System.out.println("Product id: " + id + "\nProduct name: " + name + "\nProduct price: " + price);
-    //         return;
-    //     }
+    // } else {
+    // System.out.println("Product id: " + id + "\nProduct name: " + name +
+    // "\nProduct price: " + price);
+    // return;
+    // }
     // }
 
     public void setId(int i) {
-        if (i < 0){
+        if (i < 0) {
             System.out.println("id can not be negative");
             return;
-            
         }
         id = i;
     }
 
     public void setName(String s) {
-        if (s == null){
+        if (s == null) {
             System.out.println("name can not be null");
             return;
-            
         }
         name = s;
     }
 
     public void setPrice(double p) {
-        if (p <= 0){
+        if (p <= 0) {
             System.out.println("price must be positive");
             return;
-            
-        } 
+        }
         price = p;
     }
 
-    public int getId(){ //getter means return value
+    public int getId() { // getter means return value
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return price;
     }
 }
---
+++ L7Constructors/DefaultConstructors.java
@@ -0,0 +1,12 @@
+package OPPS.L7Constructors;
+
+public class DefaultConstructors {
+    class Account{
+        private int accId;
+        private String name;
+        private double balance;
+        public Account(){
+            System.out.println("constructor called..........");
+        }
+    }
+}
